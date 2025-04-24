475212af8836535f468af072c7d4a5b5ff06e863
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-04-25 08:16:07 +0900

    0425 Add PartyClass

--- HDproject/Assets/00_Scripts/Class/Party.cs
+++ HDproject/Assets/00_Scripts/Class/Party.cs
@@ -1,16 +1,17 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Party : MonoBehaviour
+public class Party
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int partyID { get; private set; }
+    public Photon.Realtime.Player leader { get; private set; }
+    public List<Photon.Realtime.Player> members { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public Party(int partyID, Photon.Realtime.Player leader)
     {
-        
+        partyID = this.partyID;
+        leader = this.leader;
+        members = new List<Photon.Realtime.Player>() { leader };
     }
 }
