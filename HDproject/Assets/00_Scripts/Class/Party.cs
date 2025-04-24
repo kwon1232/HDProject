2dc8730ead88540c95a929ab60151436bacd1b3e
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-04-25 08:19:55 +0900

    Party Class member 관리 함수 제작

--- HDproject/Assets/00_Scripts/Class/Party.cs
+++ HDproject/Assets/00_Scripts/Class/Party.cs
@@ -1,17 +1,53 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Unity.VisualScripting;
 
 public class Party
 {
     public int partyID { get; private set; }
-    public Photon.Realtime.Player leader { get; private set; }
-    public List<Photon.Realtime.Player> members { get; private set; }
+    public Player leader { get; private set; }
+    public List<Player> members { get; private set; }
 
-    public Party(int partyID, Photon.Realtime.Player leader)
+    public Party(int partyID, Player leader)
     {
         partyID = this.partyID;
         leader = this.leader;
-        members = new List<Photon.Realtime.Player>() { leader };
+        members = new List<Player>() { leader };
+    }
+
+    public bool AddMember(Player player)
+    {
+        if(!members.Contains(player))
+        {
+            members.Add(player);
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveMember(Player player)
+    {
+        if(members.Contains(player))
+        {
+            members.Remove(player);
+            if (player == leader && members.Count > 0)
+            {
+                leader = members[0];
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsMember(Player player)
+    {
+        return members.Contains(player);
+    }
+
+    public void DisbandParty()
+    {
+        members.Clear();
     }
 }
