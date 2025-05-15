a06483b93084b13da4bd7a067a0d11da13ca3aaf
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2025-05-15 21:22:20 +0900

    AHUD 상속

--- HDproject/Assets/00_Scripts/FrameWork/HUD/SlateWidget/SSlateWidget.cs
+++ HDproject/Assets/00_Scripts/FrameWork/HUD/SlateWidget/SSlateWidget.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class SSlateWidget : MonoBehaviour
+public class SSlateWidget : AHUD
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
